fc5edbcc66152690f46817bef21396ac9e53cdef
Thakur Dheerender <dthakur@walmartlabs.com>
2016-05-12 00:19:43 -0700

    udates on listenser

--- app/src/main/java/com/codepath/dkthaku/todo/MainActivity.java
+++ app/src/main/java/com/codepath/dkthaku/todo/MainActivity.java
@@ -27,29 +27,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvItems);
-       items = new ArrayList<>();
+       //items = new ArrayList<>();
         readItems();
-        itemsAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, items);
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
+      // items.add("First Item");
+        //  items.add("Second Item");
+        Log.d(TAG, "onCreate: Items list"+items);
         setupListViewListener();
     }
 
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String iTemTxt = etNewItem.getText().toString();
-        Log.d(TAG, "onAddItem: "+iTemTxt);
+        Log.d(TAG, "onAddItem: iTemTxt "+iTemTxt);
         itemsAdapter.add(iTemTxt);
         etNewItem.setTag("");
         writeItems();
     }
 
-    private void setupListViewListener(){
-        Log.d(TAG, "setupListViewListener: "+items);
+    private void setupListViewListener1(){
+        Log.d(TAG, "setupListViewListener:Items "+items);
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
             public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id){
+                Log.d(TAG, "setupListViewListener. onItemLongClick: @@@@@@@"+pos);
                 items.remove(pos);
                 itemsAdapter.notifyDataSetChanged();
                 writeItems();
@@ -66,6 +68,31 @@ public class MainActivity extends AppCompatActivity {
         }catch (IOException e){
             items= new ArrayList<String>();
             e.printStackTrace();
+        }
+    }
+    private void setupListViewListener() {
+        Log.d(TAG, "setupListViewListener:Items in "+items);
+        try {
+            lvItems.setOnItemLongClickListener(
+                    new AdapterView.OnItemLongClickListener() {
+                        @Override
+                        public boolean onItemLongClick(AdapterView<?> adapter,
+                                                       View item, int pos, long id) {
+                            // Remove the item within array at position
+                            Log.d(TAG, "onItemLongClick: removed" + pos);
+                            items.remove(pos);
+                            // Refresh the adapter
+                            itemsAdapter.notifyDataSetChanged();
+                            // Return true consumes the long click event (marks it handled)
+                            writeItems(); // <---- Add this line
+                            Log.d(TAG, "onItemLongClick: After writes");
+                            itemsAdapter.notifyDataSetChanged();
+                            return true;
+                        }
+
+                    });
+        }catch (Exception e){  e.printStackTrace();
+
         }
     }
     private void writeItems(){
